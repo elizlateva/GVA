c5e24453a026888d797b37f46bf3fa09b10320c0
Elena Zlateva <elena.zlateva84@gmail.com>
2017-10-19 16:42:54 +0300

    adding jQuery plugin for scrolling

--- Script/script.js
+++ Script/script.js
@@ -2,7 +2,19 @@ jQuery(document).ready(function() {
     jQuery('.toggle-nav').click(function(e) {
         jQuery(this).toggleClass('active');
         jQuery('.menu ul').slideToggle();
-        jQuery('.select-lang').slideToggle();
         e.preventDefault();
+    });
+});
+
+$(function() {
+
+    $("section").css({
+        "height": $("section").height()
+    });
+
+    $.scrollify({
+        section: "section",
+        sectionName: "",
+        offset: -126
     });
 });
