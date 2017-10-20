f237c13c9a292da6b0006c754cf3174a4e68a503
Elena Zlateva <elena.zlateva84@gmail.com>
2017-10-20 11:11:16 +0300

    changing jQuery settings

--- Script/script.js
+++ Script/script.js
@@ -1,19 +1,24 @@
 jQuery(document).ready(function() {
-    jQuery('.toggle-nav').click(function(e) {
-        jQuery(this).toggleClass('active');
-        jQuery('.menu ul').slideToggle();
+    $('.toggle-nav').click(function(e) {
+        $(this).toggleClass('active');
+        $('.menu ul').slideToggle();
         e.preventDefault();
     });
 });
 
 $(function() {
-    // $("section").css({
-    //     "height": $("section").height()
-    // });
+
     $.scrollify({
         section: "section",
         sectionName: false,
         interstitialSection: "nav,footer",
-        offset: -126
+        before: function(i, sections) {
+            if (i < 2) {
+                $("nav").css({ "background": "rgba(0,0,0,0.55)" });
+            } else {
+                $("nav").css({ "background": "#333" });
+                $.scrollify.setOptions({ offset: -126 })
+            }
+        }
     });
 });
